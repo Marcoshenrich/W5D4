2f937ba9cbdb5513d6de3cef453300a8b504cace
Marcos <jmarcoshenrich@gmail.com>
2022-11-03 15:12:01 -0700

    associations part 1

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,30 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    validates :student, uniqueness{scope: :course}
+
+    belongs_to :course,
+        primary: :id,
+        foreign_key: :course_id
+        class_name: :Course
+
+    belongs_to :users,
+        primary: :id,
+        foreign_key: :student_id
+        class_name: :User
+
+    #if a table contains a foreign key referencing another table, it is a belongs to association
+    # the foreign key in the association should match
+
+
+
+
 end
