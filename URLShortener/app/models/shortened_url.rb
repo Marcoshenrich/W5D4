5e16616db7af060aa65463257e529453e13d491d
Marcos <jmarcoshenrich@gmail.com>
2022-11-03 18:06:05 -0700

    shortURL component under construction

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -11,6 +11,26 @@
 #
 
 class ShortenedUrl < ApplicationRecord
-    validates :long_url, :short_url, :user_id, presence: true
-    validates :short_url, uniqueness: true
+    validates :long_url, :user_id, presence: true
+
+    # ShortenedUrl::random_code that uses SecureRandom::urlsafe_base64 to generate a random 16-byte string.
+
+    if new_record?(self)  #new_record? Returns true if this object hasn't been saved yet – that is, a record for the object doesn't exist in the database yet; otherwise, returns false.
+        after_initialize do |shortenedurl|
+            short_url = ShortenedUrl.random_code
+        end
+    end
+
+    def self.random_code
+        exist_check = true
+        temp_url = 0
+
+        while exist_check
+        temp_url = SecureRandom.urlsafe_base64(22)
+        exist_check = ShortenedUrl.exists?(:short_url => temp_url)
+        end
+
+        temp_url
+    end
+
 end
